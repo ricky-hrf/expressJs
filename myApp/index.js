fe028b3e7f23e00afe2b55a8aecbc4135af1626d
Ricky Harefa <114171685+ricky-hrf@users.noreply.github.com>
2025-05-08 16:17:35 +0700

    Routing Basic

--- myApp/index.js
+++ myApp/index.js
@@ -3,7 +3,22 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.send('ahihih')
+})
+
+// method dengan perameter
+app.get('/users/:userId', (req, res) => {
+  res.send(req.params)
+})
+
+// random text
+app.get('/random.text', (req, res) => {
+  res.send('random.text')
+})
+
+// method post atau mengirim data melalui route
+app.post('/kirim', (req, res) => {
+  res.send('sudah bisa buat post')
 })
 
 app.listen(port, () => {
