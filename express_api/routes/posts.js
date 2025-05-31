2da7e5f0f19e4d00b6f5d62a80fc387bd9635a33
Ricky Harefa <114171685+ricky-hrf@users.noreply.github.com>
2025-05-31 15:13:56 +0700

    Lanjutan Configurasi Database

--- express_api/routes/posts.js
+++ express_api/routes/posts.js
@@ -4,6 +4,7 @@ const { body, validationResult } = require('express-validator');
 
 //import database
 const connection = require('../config/database');
+const { contextsKey } = require('express-validator/lib/base');
 
 router.get('/', function(req, res) {
   // query
@@ -54,6 +55,88 @@ router.post('/store', [
       })
     }
   })
-})
+});
+
+// router detail isi tabel
+router.get('/(:id)', function (req, res) {
+  let id = req.params.id;
+  connection.query(`SELECT * FROM posts WHERE id = ${id}`, function (err, rows) {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: 'Internal Server Error',
+      })
+    }
+
+    if (rows.length <= 0) {
+      return res.status(404).json({
+        status: false,
+        message: 'Data Post Tidak Ditemukan',
+      })
+    } else {
+      return res.status(200).json({
+        status: true,
+        message: 'Detail Data Post',
+        data: rows[0]
+      });
+    }
+  });
+});
+
+// route untuk update data tabel
+router.patch('/update/:id', [
+  // kita validasi dulu
+  body('title').notEmpty(),
+  body('content').notEmpty()
+], (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      errors: errors.array()
+    });
+  }
+  // id post
+  let id = req.params.id;
+
+  // data post
+  let formData = {
+    title: req.body.title,
+    content: req.body.content
+  }
+
+  // update query
+  connection.query(`UPDATE posts SET ? WHERE id = ${id}`, formData, function (err, rows) {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: 'Internal Server Error',
+      });
+    } else {
+      return res.status(200).json({
+        status: true,
+        message: 'Update Data Successfully!'
+      });
+    }
+  });
+});
+
+// route untuk delete data pada tabel
+router.delete('/delete/(:id)', function (req, res) {
+  let id = req.params.id;
+  connection.query(`DELETE FROM posts WHERE id = ${id}`, function (err, rows) {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: 'Internal Server Error',
+      })
+    } else {
+      return res.status(200).json({
+        status: true,
+        message: 'Delete Data Successfully!',
+      })
+    }
+  })
+});
 
 module.exports = router;
