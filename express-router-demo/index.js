9a3a50aa751d79b2df283c196ac00cc319ef378d
Ricky Harefa <riskiharefa2000@gmail.com>
2026-01-01 23:23:56 +0700

    belajar logger

--- express-router-demo/index.js
+++ express-router-demo/index.js
@@ -1,34 +1,14 @@
 import express from 'express';
 import todoRoutes from './routes/todos.js';
+import log from './middlewares/logger.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(log);
+
 // todo routes
-app.use('/todos', todoRoutes);
-
-// app.get('/todos', (req, res) => {
-//   res.send('A list of todo items');
-// })
-
-// app.post('/todos', (req, res) => {
-//   res.send('create a new todo item');
-// });
-
-// app.put('/todos/:id', (req, res) => {
-//   const { id } = req.params;
-//   res.send(`update the todo item with id ${id}`);
-// });
-
-// app.delete('/todos/:id', (req, res) => {
-//   const { id } = req.params;
-//   res.send(`delete the todo item with id ${id}`);
-// });
-
-// app.get('/todos/:id', (req, res) => {
-//   const { id } = req.params;
-//   res.send(`get the todo item with id ${id}`);
-// });
+app.use('/', todoRoutes);
 
 app.listen(PORT, () => {
   console.log(`The server has run on port http://localhost:${PORT}`);
--- express-router-demo/routes/todos.js
+++ express-router-demo/routes/todos.js
@@ -2,24 +2,28 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  res.send(`halaman index`);
+});
+
+router.get('/todos', (req, res) => {
   res.send('A list of todo item');
 });
 
-router.post('/', (req, res) => {
+router.post('/todos', (req, res) => {
   res.send('create a new todo item');
 });
 
-router.put('/:id', (req, res) => {
+router.put('/todos/:id', (req, res) => {
   const { id } = req.params;
   res.send(`update the todo item with id ${id}`);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/todos/:id', (req, res) => {
   const { id } = req.params;
   res.send(`delete the todo item with id ${id}`);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/todos/:id', (req, res) => {
   const { id } = req.params;
   res.send(`get the todo item with id ${id}`);
 });
