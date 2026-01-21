5e27aa431ab90290bc17d5955329db59a42fe323
Ricky Harefa <riskiharefa2000@gmail.com>
2026-01-21 23:50:04 +0700

    perbaiki root database

--- 07_login-system/config/database.js
+++ 07_login-system/config/database.js
@@ -1,11 +1,13 @@
 import mysql from 'mysql2/promise';
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'rootd@tabAse123',
-  database: 'db_myschool',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 })
 
 export default db;
